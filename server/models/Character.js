2ceda92e6be59334382e0248174cc71a001f346b
Aldreiu <267166409@qq.com>
2020-07-22 00:21:44 +0800

    character model ok1

--- server/models/Character.js
+++ server/models/Character.js
@@ -29,7 +29,46 @@ const schema = new mongoose.Schema({
     // 种族
     race:{
         type:mongoose.Schema.Types.ObjectId,ref:'Category'
-    }
+    },
+    // 角色立绘
+    drawing:{
+        charactericon:{type:String}
+    },
+    // 技能
+    skills:[{
+        // 技能图标
+        icon:{type:String},
+        // 技能名称
+        name:{type:String},
+        // 技能介绍
+        description:{type:String}
+
+    }],
+    // 控制是否有 最终上限这样才有 item5
+    finalstart:{
+        start:{type:Boolean}
+    },
+    // 突破上限材料1   这个库之后加
+    items1:{
+
+    },
+    items2:{
+
+    },
+    items3:{
+
+    },
+    items4:{
+
+    },
+    items5:{
+
+    },
+    // 相关角色
+    partners:[{
+        // 只想本身拿角色头像
+        character:{type:mongoose.Schema.Types.ObjectId,ref:'Character'}
+    }]
 })
 
 module.exports = mongoose.model('Character', schema)
