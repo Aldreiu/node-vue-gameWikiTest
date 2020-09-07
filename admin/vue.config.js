c528cd3c6a7a536c444e681a031205cd3adcb53b
Aldreiu <267166409@qq.com>
2020-09-07 13:15:53 +0800

    sucess build

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -3,5 +3,19 @@ module.exports = {
     outputDir:__dirname + '/../server/admin',
     publicPath: process.env.NODE_ENV === 'production'
       ? '/admin/'
-      : '/'
+      : '/',
+    //清理 console
+    chainWebpack: config => {
+      config.optimization
+        .minimizer('terser')
+        .tap(args => {
+          Object.assign(args[0].terserOptions.compress, { // 生产模式 console.log 去除
+            // warnings: false , // 默认 false
+            // drop_console:  ,
+            // drop_debugger: true, // 默认也是true 
+            pure_funcs: ['console.log']
+          })
+          return args
+        })
+    }
   }
--- web/vue.config.js
+++ web/vue.config.js
@@ -4,4 +4,19 @@ module.exports = {
     // publicPath: process.env.NODE_ENV === 'production'
     //   ? '/'
     //   : '/'
+
+    // 清理 console
+    chainWebpack: config => {
+      config.optimization
+        .minimizer('terser')
+        .tap(args => {
+          Object.assign(args[0].terserOptions.compress, { // 生产模式 console.log 去除
+            // warnings: false , // 默认 false
+            // drop_console:  ,
+            // drop_debugger: true, // 默认也是true 
+            pure_funcs: ['console.log']
+          })
+          return args
+        })
+    }
   }
